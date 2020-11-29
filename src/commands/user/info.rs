9ff985114b813269cc277a98b95072b93392b2f2
Gautam.A <gautam.abhyankar@gmail.com>
2020-11-29 21:58:28 +0530

    Version bump

--- src/commands/user/info.rs
+++ src/commands/user/info.rs
@@ -137,7 +137,7 @@ pub async fn botinfo(ctx: &Context, msg: &Message, _args: Arguments<'fut>) -> Co
             .unwrap()
             .inline(),
         )
-        .field(EmbedFieldBuilder::new("Version", "2.7.0").unwrap().inline())
+        .field(EmbedFieldBuilder::new("Version", "2.7.1").unwrap().inline())
         .field(EmbedFieldBuilder::new("Language", "Rust").unwrap().inline())
         .field(
             EmbedFieldBuilder::new("Shards", ctx.bot_config.total_shards.to_string())
