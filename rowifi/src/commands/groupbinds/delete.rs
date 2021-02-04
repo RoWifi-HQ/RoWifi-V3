7d05b1670d08e73fd683f90e8de381b8ebd716d5
Gautam.A <gautam.abhyankar@gmail.com>
2021-02-05 01:56:56 +0530

    Add the groupbinds to the new framework

--- rowifi/src/commands/groupbinds/delete.rs
+++ rowifi/src/commands/groupbinds/delete.rs
@@ -1,38 +1,26 @@
-use rowifi_framework::prelude::*;
+use framework_new::prelude::*;
+use mongodb::bson::doc;
 
-pub static GROUPBINDS_DELETE_OPTIONS: CommandOptions = CommandOptions {
-    perm_level: RoLevel::Admin,
-    bucket: None,
-    names: &["delete", "d", "remove"],
-    desc: Some("Command to delete a groupbind"),
-    usage: Some("groupbinds delete <Group Id>"),
-    examples: &["groupbinds delete 3108077", "gb remove 5581309"],
-    min_args: 1,
-    hidden: false,
-    sub_commands: &[],
-    group: None,
-};
+#[derive(FromArgs)]
+pub struct GroupbindsDeleteArguments {
+    #[arg(help = "The ID of the groupbind to delete", rest)]
+    pub id: String,
+}
 
-pub static GROUPBINDS_DELETE_COMMAND: Command = Command {
-    fun: groupbinds_delete,
-    options: &GROUPBINDS_DELETE_OPTIONS,
-};
-
-#[command]
 pub async fn groupbinds_delete(
-    ctx: &Context,
-    msg: &Message,
-    args: Arguments<'fut>,
+    ctx: CommandContext,
+    args: GroupbindsDeleteArguments,
 ) -> CommandResult {
-    let guild_id = msg.guild_id.unwrap();
+    let guild_id = ctx.guild_id.unwrap();
     let guild = ctx
+        .bot
         .database
         .get_guild(guild_id.0)
         .await?
         .ok_or(RoError::Command(CommandError::NoRoGuild))?;
 
     let mut groups_to_delete = Vec::new();
-    for arg in args {
+    for arg in args.id.split_ascii_whitespace() {
         if let Ok(r) = arg.parse::<i64>() {
             groups_to_delete.push(r);
         }
@@ -56,19 +44,18 @@ pub async fn groupbinds_delete(
             .unwrap()
             .build()
             .unwrap();
-        let _ = ctx
+        ctx.bot
             .http
-            .create_message(msg.channel_id)
+            .create_message(ctx.channel_id)
             .embed(embed)
             .unwrap()
-            .await;
+            .await?;
         return Ok(());
     }
 
-    let filter = bson::doc! {"_id": guild.id};
-    let update =
-        bson::doc! {"$pull": {"GroupBinds": {"GroupId": {"$in": binds_to_delete.clone()}}}};
-    ctx.database.modify_guild(filter, update).await?;
+    let filter = doc! {"_id": guild.id};
+    let update = doc! {"$pull": {"GroupBinds": {"GroupId": {"$in": binds_to_delete.clone()}}}};
+    ctx.bot.database.modify_guild(filter, update).await?;
 
     let e = EmbedBuilder::new()
         .default_data()
@@ -80,9 +67,9 @@ pub async fn groupbinds_delete(
         .unwrap()
         .build()
         .unwrap();
-    let _ = ctx
+    ctx.bot
         .http
-        .create_message(msg.channel_id)
+        .create_message(ctx.channel_id)
         .embed(e)
         .unwrap()
         .await?;
@@ -93,13 +80,13 @@ pub async fn groupbinds_delete(
         .collect::<String>();
     let log_embed = EmbedBuilder::new()
         .default_data()
-        .title(format!("Action by {}", msg.author.name))
+        .title(format!("Action by {}", ctx.author.name))
         .unwrap()
         .description("Custom Bind Deletion")
         .unwrap()
         .field(EmbedFieldBuilder::new("Binds Deleted", ids_str).unwrap())
         .build()
         .unwrap();
-    ctx.logger.log_guild(ctx, guild_id, log_embed).await;
+    ctx.log_guild(guild_id, log_embed).await;
     Ok(())
 }
