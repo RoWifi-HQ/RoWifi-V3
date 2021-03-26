4ddfdc6032ce1a4a36ef9de2117fc40e79fdbcc9
AsianIntel <gautam.abhyankar@gmail.com>
2021-03-26 17:25:32 +0530

    Add verify remove, view

--- rowifi/src/commands/user/verify/manage.rs
+++ rowifi/src/commands/user/verify/manage.rs
@@ -209,3 +209,106 @@ pub async fn verify_default(ctx: CommandContext, args: VerifyArguments) -> Comma
 
     Ok(())
 }
+
+pub async fn verify_delete(ctx: CommandContext, args: VerifyArguments) -> CommandResult {
+    let mut user = match ctx.bot.database.get_user(ctx.author.id.0).await? {
+        Some(u) => u.as_ref().clone(),
+        None => {
+            let embed = EmbedBuilder::new()
+                .default_data()
+                .title("User Not Verified")
+                .unwrap()
+                .description("You are not verified. Please use `verify` to link your account")
+                .unwrap()
+                .color(Color::Red as u32)
+                .unwrap()
+                .build()
+                .unwrap();
+            ctx.bot
+                .http
+                .create_message(ctx.channel_id)
+                .embed(embed)
+                .unwrap()
+                .await?;
+            return Ok(());
+        }
+    };
+
+    let roblox_username = match args.username {
+        Some(r) => r,
+        None => await_reply("Enter your Roblox Username", &ctx).await?,
+    };
+
+    let roblox_id = match ctx
+        .bot
+        .roblox
+        .get_id_from_username(&roblox_username)
+        .await?
+    {
+        Some(r) => r,
+        None => {
+            let e = EmbedBuilder::new()
+                .default_data()
+                .color(Color::Red as u32)
+                .unwrap()
+                .title("Verification Process Failed")
+                .unwrap()
+                .description("Invalid Roblox Username. Please try again.")
+                .unwrap()
+                .build()
+                .unwrap();
+            ctx.bot
+                .http
+                .create_message(ctx.channel_id)
+                .embed(e)
+                .unwrap()
+                .await?;
+            return Ok(());
+        }
+    };
+
+    let account_index = match user.alts.iter().position(|r| *r == roblox_id) {
+        Some(i) => i,
+        None => {
+            let e = EmbedBuilder::new()
+                .default_data()
+                .color(Color::Red as u32)
+                .unwrap()
+                .title("Verification Process Failed")
+                .unwrap()
+                .description("The provided username is not linked to your discord account or is your default account")
+                .unwrap()
+                .build()
+                .unwrap();
+            ctx.bot
+                .http
+                .create_message(ctx.channel_id)
+                .embed(e)
+                .unwrap()
+                .await?;
+            return Ok(());
+        }
+    };
+    user.alts.remove(account_index);
+    ctx.bot
+        .database
+        .delete_linked_users(ctx.author.id.0, user.roblox_id)
+        .await?;
+    ctx.bot.database.add_user(user, true).await?;
+    let embed = EmbedBuilder::new()
+        .default_data()
+        .color(Color::DarkGreen as u32)
+        .unwrap()
+        .title("Account Unlinking Successful")
+        .unwrap()
+        .build()
+        .unwrap();
+    ctx.bot
+        .http
+        .create_message(ctx.channel_id)
+        .embed(embed)
+        .unwrap()
+        .await?;
+
+    Ok(())
+}
