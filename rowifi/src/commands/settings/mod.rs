ff265f28078c240e43c09fc3de58c34c99a522dc
AsianIntel <gautam.abhyankar@gmail.com>
2022-01-17 00:26:56 +0530

    convert all functional roles commands to subcommand groups

--- rowifi/src/commands/settings/mod.rs
+++ rowifi/src/commands/settings/mod.rs
@@ -11,13 +11,15 @@ mod verify;
 use itertools::Itertools;
 use rowifi_framework::prelude::*;
 
-use admin::admin;
-use bypass::bypass;
+use admin::{admin_add, admin_remove, admin_set, admin_view};
+use bypass::{bypass_add, bypass_remove, bypass_set, bypass_view};
 use functional::functional;
 use log::log_channel;
 use misc::{blacklist_action, settings_prefix, toggle_commands};
-use nickname_bypass::nickname_bypass;
-use trainer::trainer;
+use nickname_bypass::{
+    nickname_bypass_add, nickname_bypass_remove, nickname_bypass_set, nickname_bypass_view,
+};
+use trainer::{trainer_add, trainer_remove, trainer_set, trainer_view};
 use update::update_on_join;
 use verify::{
     settings_verification_add, settings_verification_remove, settings_verified_add,
@@ -95,29 +97,141 @@ pub fn settings_config(cmds: &mut Vec<Command>) {
         .sub_command(settings_verified_remove_cmd)
         .handler(settings_view);
 
+    let admin_add_cmd = Command::builder()
+        .level(RoLevel::Admin)
+        .names(&["add"])
+        .description("Command to add custom admin roles")
+        .handler(admin_add);
+
+    let admin_remove_cmd = Command::builder()
+        .level(RoLevel::Admin)
+        .names(&["remove"])
+        .description("Command to remove custom admin roles")
+        .handler(admin_remove);
+
+    let admin_set_cmd = Command::builder()
+        .level(RoLevel::Admin)
+        .names(&["set"])
+        .description("Command to set custom admin roles")
+        .handler(admin_set);
+
+    let admin_view_cmd = Command::builder()
+        .level(RoLevel::Admin)
+        .names(&["view"])
+        .description("Command to view custom admin roles")
+        .handler(admin_view);
+
     let settings_admin_cmd = Command::builder()
         .level(RoLevel::Admin)
         .names(&["admin"])
-        .description("Command to interact with the custom admin roles")
-        .handler(admin);
+        .description("Module to interact with the custom admin roles")
+        .sub_command(admin_add_cmd)
+        .sub_command(admin_remove_cmd)
+        .sub_command(admin_set_cmd)
+        .sub_command(admin_view_cmd)
+        .handler(admin_view);
+
+    let trainer_add_cmd = Command::builder()
+        .level(RoLevel::Admin)
+        .names(&["add"])
+        .description("Command to add custom trainer roles")
+        .handler(trainer_add);
+
+    let trainer_remove_cmd = Command::builder()
+        .level(RoLevel::Admin)
+        .names(&["remove"])
+        .description("Command to remove custom trainer roles")
+        .handler(trainer_remove);
+
+    let trainer_set_cmd = Command::builder()
+        .level(RoLevel::Admin)
+        .names(&["set"])
+        .description("Command to set custom trainer roles")
+        .handler(trainer_set);
+
+    let trainer_view_cmd = Command::builder()
+        .level(RoLevel::Admin)
+        .names(&["view"])
+        .description("Command to view custom trainer roles")
+        .handler(trainer_view);
 
     let settings_trainer_cmd = Command::builder()
         .level(RoLevel::Admin)
         .names(&["trainer"])
-        .description("Command to interact with the custom trainer roles")
-        .handler(trainer);
+        .description("Module to interact with the custom trainer roles")
+        .sub_command(trainer_add_cmd)
+        .sub_command(trainer_remove_cmd)
+        .sub_command(trainer_set_cmd)
+        .sub_command(trainer_view_cmd)
+        .handler(trainer_view);
+
+    let bypass_add_cmd = Command::builder()
+        .level(RoLevel::Admin)
+        .names(&["add"])
+        .description("Command to add custom bypass roles")
+        .handler(bypass_add);
+
+    let bypass_remove_cmd = Command::builder()
+        .level(RoLevel::Admin)
+        .names(&["remove"])
+        .description("Command to remove custom bypass roles")
+        .handler(bypass_remove);
+
+    let bypass_set_cmd = Command::builder()
+        .level(RoLevel::Admin)
+        .names(&["set"])
+        .description("Command to set custom bypass roles")
+        .handler(bypass_set);
+
+    let bypass_view_cmd = Command::builder()
+        .level(RoLevel::Admin)
+        .names(&["view"])
+        .description("Command to view custom bypass roles")
+        .handler(bypass_view);
 
     let settings_bypass_cmd = Command::builder()
         .level(RoLevel::Admin)
         .names(&["bypass"])
-        .description("Command to interact with the custom bypass roles")
-        .handler(bypass);
+        .description("Module to interact with the custom bypass roles")
+        .sub_command(bypass_add_cmd)
+        .sub_command(bypass_remove_cmd)
+        .sub_command(bypass_set_cmd)
+        .sub_command(bypass_view_cmd)
+        .handler(bypass_view);
+
+    let nickname_bypass_add_cmd = Command::builder()
+        .level(RoLevel::Admin)
+        .names(&["add"])
+        .description("Command to add custom nickname bypass roles")
+        .handler(nickname_bypass_add);
+
+    let nickname_bypass_remove_cmd = Command::builder()
+        .level(RoLevel::Admin)
+        .names(&["remove"])
+        .description("Command to remove custom nickname bypass roles")
+        .handler(nickname_bypass_remove);
+
+    let nickname_bypass_set_cmd = Command::builder()
+        .level(RoLevel::Admin)
+        .names(&["set"])
+        .description("Command to set custom nickname bypass roles")
+        .handler(nickname_bypass_set);
+
+    let nickname_bypass_view_cmd = Command::builder()
+        .level(RoLevel::Admin)
+        .names(&["view"])
+        .description("Module to view custom nickname bypass roles")
+        .handler(nickname_bypass_view);
 
     let settings_nickname_bypass_cmd = Command::builder()
         .level(RoLevel::Admin)
         .names(&["nickname-bypass", "nb"])
-        .description("Command to interact with the custom nickname bypass roles")
-        .handler(nickname_bypass);
+        .description("Module to interact with the custom nickname bypass roles")
+        .sub_command(nickname_bypass_add_cmd)
+        .sub_command(nickname_bypass_remove_cmd)
+        .sub_command(nickname_bypass_set_cmd)
+        .sub_command(nickname_bypass_view_cmd)
+        .handler(nickname_bypass_view);
 
     let log_channel_cmd = Command::builder()
         .level(RoLevel::Admin)
